f88e6445f72f88660120aa4e2c8a093fc5aacf00
manuelsavino <manuelsavino@gmail.com>
2021-06-10 11:31:26 -0400

    add default state from edit page

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -109,14 +109,63 @@ const Index = () => {
   useEffect(() => {
     if (localStorage.getItem('heroOptions') != null) {
       setHeroOptions(JSON.parse(localStorage.getItem('heroOptions')));
+    } else {
+      setHeroOptions({
+        heroImg:
+          'https://images.unsplash.com/photo-1541789094913-f3809a8f3ba5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        headline: 'Best event ever, coming soon to a screen near you',
+        headlineColor: '#4F46E5',
+        ctaCopy: 'Register Now',
+        ctaBg: '#4F46E5',
+        ctaCopyColor: '#FFFFFF',
+        heroCopy:
+          'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.',
+        textSize: 'text-5xl',
+      });
     }
     if (localStorage.getItem('speakers') != null) {
       setSpeakers(JSON.parse(localStorage.getItem('speakers')));
+    } else {
+      setSpeakers({
+        1: {
+          speakerTitle: 'Desert Expert',
+          speakerName: 'Jose Smith',
+          speakerImage:
+            'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+        },
+        2: {
+          speakerTitle: 'Speaker',
+          speakerName: 'Joanna Smith',
+          speakerImage:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+        },
+      });
     }
     if (localStorage.getItem('sponsors') != null) {
       setSponsors(JSON.parse(localStorage.getItem('sponsors')));
+    } else {
+      setSponsors({
+        1: {
+          sponsorLogo:
+            'https://www.meetingplay.com/hubfs/website-homepage-may-2020/logo.png',
+        },
+        2: {
+          sponsorLogo:
+            'https://www.carnival.com/-/media/Images/Header2013/carnival-logo-png',
+        },
+        3: {
+          sponsorLogo:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Spirit_Airlines_logo.svg/1200px-Spirit_Airlines_logo.svg.png',
+        },
+      });
     }
   }, []);
+
+  useEffect(() => {
+    saveToLS('sponsors', JSON.stringify(sponsors));
+    saveToLS('speakers', JSON.stringify(speakers));
+    saveToLS('heroOptions', JSON.stringify(heroOptions));
+  }, [sponsors, speakers, heroOptions]);
   return (
     <div className='h-screen flex overflow-hidden'>
       <div className='flex flex-col w-0 flex-1 overflow-hidden'>
